1c97b5f9b02ef4ebcc8cce960bcbfcf0bef0396a
pranavkuruvella <pranavkuruvella@gmail.com>
2025-09-17 19:01:05 +0530

    basic setup - db connection , started MVC structure , some changes in env

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+
+// Signup route
+export const signup = async(req, res) => {
+  res.send("Signup route");
+};
+
+// Login route
+export const login = async(req, res) => {
+  res.send("Login route");
+};
+
+// Logout route
+export const logout = (req, res) => {
+  res.send("Logout route");
+};
