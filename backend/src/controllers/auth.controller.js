28c41c680d9a0997d3a3e135450868aece5882a3
pranavkuruvella <pranavkuruvella@gmail.com>
2025-09-22 17:50:22 +0530

    added login and signup route and also for signup user is created in Stream too

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken"
 
@@ -21,12 +22,25 @@ export const signup = async (req, res) => {
     const idx = Math.floor(Math.random() * 100) + 1 //generates random num btw 1 to 100
     const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png` //gives random avatars this links need idx from 1 to 100 which gives 100 different avatars
 
-  const newUser = await User.create({ //creates new User and also saves it into db
+    const newUser = await User.create({ //creates new User and also saves it into db
       email,
       fullName,
       password,
       profilePic: randomAvatar,
     })
+
+    //saving the new User into stream 
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullName,
+        image: newUser.profilePic || "https://avatar.iran.liara.run/public/1.png"
+      })
+      console.log(`Stream User created for ${newUser.fullName} with id ${newUser._id}`)
+    } catch (error) {
+      console.log(`error creating Stream User!!`, error)
+    }
+
 
     const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET_KEY, {
       expiresIn: "7d"
@@ -41,7 +55,7 @@ export const signup = async (req, res) => {
 
     res.status(201).json({ success: true, user: newUser })
   } catch (error) {
-    console.log('error in signup controller',error)
+    console.log('error in signup controller', error)
     res.status(500).json({ message: "Internal server error" })
   }
 
@@ -49,10 +63,41 @@ export const signup = async (req, res) => {
 
 // Login route
 export const login = async (req, res) => {
-  res.send("Login route");
+
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) return res.status(400).json({ message: "All fields required" });
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ message: "Invalid email or password" });
+
+    const isPasswordCorrect = await user.matchPassword(password);
+    if (!isPasswordCorrect) return res.status(401).json({ message: "Invalid email or password" });
+
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d"
+    })
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, //prevents XSS attack,
+      sameSite: 'strict', //prevents CRSF attacks
+      secure: process.env.NODE_ENV === "production",
+    })
+
+    res.status(201).json({ success: true, user })
+  } catch (error) {
+    console.log('error in login controller', error)
+    res.status(500).json({ message: "Internal server error" })
+  }
+
 };
 
 // Logout route
 export const logout = (req, res) => {
-  res.send("Logout route");
+
+  res.clearCookie("jwt")
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 };
---
+++ backend/src/lib/stream.js
@@ -0,0 +1,26 @@
+import {StreamChat} from "stream-chat"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const api_key = process.env.STREAM_API_KEY
+const api_secret = process.env.STREAM_API_SECRET
+
+if(!api_key,!api_secret) console.log('Stream API keys r missing')
+
+const streamClient = StreamChat.getInstance(api_key,api_secret)
+
+export const upsertStreamUser = async (userdata) => { // saves the user to stream application
+  try {
+    if (!userdata || !userdata.id) {
+      throw new Error('User ID is required for upserting a Stream user');
+    }
+    await streamClient.upsertUser(userdata);
+    return userdata;
+  } catch (error) {
+    console.log('error in upsertStreamUser', error);
+    throw error;
+  }
+}
+
+export const generateStreamToken = async (userId)=>{}
