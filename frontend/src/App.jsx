7b5b96a096ea235c0090f5cc6a7f8eb42aaddf61
pranavkuruvella <pranavkuruvella@gmail.com>
2025-09-30 10:36:35 +0530

    built singup page but it have issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router'
+import { Navigate } from 'react-router'
 
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
@@ -8,25 +9,49 @@ import OnBoardingPage from './pages/OnBoardingPage'
 import ChatPage from './pages/ChatPage'
 import CallPage from './pages/CallPage'
 import NotificationPage from './pages/NotificationPage'
-import toast, {Toaster} from 'react-hot-toast' //alert ot notification chupinchadaniki
-import {useQuery} from '@tanstack/react-query'
+import toast, { Toaster } from 'react-hot-toast' //alert ot notification chupinchadaniki
+import { useQuery } from '@tanstack/react-query'
 import { axiosInstance } from './lib/axios.js'
 
 const App = () => {
+
+  const { data: authData, isLoading, isError } = useQuery({
+    queryKey: ['authUser'],
+    queryFn: async () => {
+      const res = await axiosInstance.get('/me')
+      return res.data
+    },
+    retry: false, //by default tanStack 3 times try chesthundhi if res = err while fetching
+  })
+  const authUser = authData?.user // /me route degara user:req.user ani rasam anuduke .user rayali eekada
+  //using authUser we can authenticate routes
+  console.log(authUser)
   return (
-    <div className='h-screen'>
+    <div className="h-screen" data-theme="night">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifications" element={<NotificationPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/onboarding" element={<OnBoardingPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+
+        <Route
+          path="/notifications"
+          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/call"
+          element={authUser ? <CallPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/chat"
+          element={authUser ? <ChatPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/onboarding"
+          element={authUser ? <OnBoardingPage /> : <Navigate to="/login" />}
+        />
       </Routes>
-      <Toaster/>
     </div>
-  )
+  );
 }
 
 export default App
